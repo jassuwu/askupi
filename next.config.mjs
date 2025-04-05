b73ead619cb16eb1c675c206dd583dc667cfd2d2
jassuwu <karthickprnv@gmail.com>
2025-04-05 20:44:11 +0530

    chore: add standalone output and transpile packages to Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ jiti.import("./src/env.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
+    transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
 
 export default nextConfig;
